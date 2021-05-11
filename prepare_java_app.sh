557f18fb3ab7cbab0c374d3efb9685397ff4d7fe
BuildTools <git@kelm.net>
2021-05-11 18:36:01 +0000

    fix: test for existing file before linking

--- prepare_java_app.sh
+++ prepare_java_app.sh
@@ -2,7 +2,7 @@
 # Create links for all plugins in server directory.
 mkdir -p "${SERVER_DIR}/plugins"
 for filename in ${PLUGINS_JAR_DIR}/*.jar ; do
-    test -e "$filename" || continue
+    test -e "${SERVER_DIR}/plugins/$(basename $filename)" || continue
     echo prepare_java_app: Linking plugin jar $filename to ${SERVER_DIR}/plugins/$(basename $filename)
     ln -s "${filename}" "${SERVER_DIR}/plugins/$(basename $filename)"
 done
